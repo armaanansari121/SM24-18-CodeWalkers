11653e941f5f9715e015d8094a441b25e19cb7e9
PRakhar <srivastavaprakhar3010@gmail.com>
2024-08-23 15:00:49 +0530

    added logo

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -14,12 +14,9 @@ const Sidebar = () => {
   return (
     <div className="w-64 bg-slate-100 shadow-md h-screen flex flex-col">
       <div className="flex justify-between items-center p-4 border-b border-gray-200">
-        <Image src="/logo.png" alt="Platform Logo" width={40} height={40} className="rounded-full" />
-        <button className="text-gray-500 hover:text-gray-700 transition-colors duration-200">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
+
+        <Image src="/logo.png" alt="Platform Logo" width={900} height={40} quality={100}  />
+
       </div>
       <nav className="flex-1 overflow-y-auto py-4">
         <Link href="/feeds" className="flex items-center px-4 py-2 text-gray-700 hover:bg-blue-100 hover:text-blue-600 transition-colors duration-200">
