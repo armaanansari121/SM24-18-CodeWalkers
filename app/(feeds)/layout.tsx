683e6cab69a0ddbbb421db2060f7c6f0375846fa
Harsh kasana <bt22ece106@gmail.com>
2024-08-23 20:27:46 +0530

    page added

--- app/(feeds)/layout.tsx
+++ app/(feeds)/layout.tsx
@@ -1,5 +1,6 @@
 // app/feeds/layout.tsx
 
+import ConnectWallet from "../_components/ConnectWallet";
 import SearchBar from "../_components/SearchBar";
 import Sidebar from "../_components/Sidebar";
 
@@ -10,11 +11,12 @@ export default function FeedsLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex h-screen bg-gray-100">
+    <div className="flex h-screen bg-slate-300">
       <Sidebar />
       <main className="flex-1 overflow-y-auto">
         <div className="max-w-3xl mx-auto py-6 sm:px-6 lg:px-8">
           <SearchBar />
+          <ConnectWallet/>
           {children}
         </div>
       </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Swartz",
-  description: "",
+  description: "Decentralized social media platform",
 };
 
 export default function RootLayout({
