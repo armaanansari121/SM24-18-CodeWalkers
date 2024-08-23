65df6f9d9944cf9fa3d86dc240a55b957ea727ac
Harsh kasana <bt22ece106@gmail.com>
2024-08-23 21:28:28 +0530

    Improve layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import  { ContractProvider } from "./_contexts/ContractContext";
 
+import Sidebar from "./_components/Sidebar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +20,20 @@ export default function RootLayout({
   return (
     <html lang="en">
     <ContractProvider>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="flex h-screen bg-slate-300">
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto">
+        <div className="max-w-3xl mx-auto py-12 sm:px-6 lg:px-4">
+          
+          {/* <ConnectWallet/> */}
+          {/* <ConnectButton client={client} /> */}
+          {children}
+        </div>
+      </main>
+    </div>
+        
+      </body>
     
     </ContractProvider>
     </html>
