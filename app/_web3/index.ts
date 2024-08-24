98ccad506aaaa41bdbb8056917b6f59d6a03e933
Harsh kasana <bt22ece106@gmail.com>
2024-08-24 11:10:42 +0530

    Added new Governance contract

--- app/_web3/index.ts
+++ app/_web3/index.ts
@@ -1,3 +1,3 @@
-export const GovernanceAddress = "0xA514eE0710422f8E02fB3e864Cd9074Dd7dFD92B";
+export const GovernanceAddress = "0x84a058bb036444f1e7EA89088B506b682744379c";
 export const contractAddress = "0x2f9F4BEE6550148EDEeDD5E0f0393726a864c36C";
 
