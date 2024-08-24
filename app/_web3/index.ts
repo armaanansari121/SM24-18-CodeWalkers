2a38448589069337236066ba9e324b06b0c70cc5
armaanansari121 <armaanm2a@gmail.com>
2024-08-24 11:11:42 +0530

    UI changes

--- app/_web3/index.ts
+++ app/_web3/index.ts
@@ -1,3 +1,2 @@
 export const GovernanceAddress = "0xA514eE0710422f8E02fB3e864Cd9074Dd7dFD92B";
-export const contractAddress = "0x2f9F4BEE6550148EDEeDD5E0f0393726a864c36C";
-
+export const contractAddress = "0x9b9E6ffB46EE3794628952b9a2876f9795F59099";
